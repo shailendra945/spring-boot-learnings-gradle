b35c2426acb63d263d00767b5d3365f9c21bc785
Shailendra.Yadav <yadav.shailendra945@gmail.com>
2023-05-29 13:00:46 +0530

    adding rest capabilities

--- FirstProject/src/main/java/com/infoiv/firstproject/FirstProjectApplication.java
+++ FirstProject/src/main/java/com/infoiv/firstproject/FirstProjectApplication.java
@@ -2,12 +2,22 @@ package com.infoiv.firstproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/helloworld")
 public class FirstProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FirstProjectApplication.class, args);
     }
 
+
+    @GetMapping
+    public String getMessage(){
+        return "Hello world!";
+    }
 }
